65643c202eab40934a58678a04dcd16e1cdefece
abc <abc@gmail.com>
2023-07-13 15:09:31 +0800

    gateio-v4, update

--- xchange-gateio-v4/src/test/java/org/knowm/xchange/gateiov4/service/GateioFuturesMarketDataServiceRawTest.java
+++ xchange-gateio-v4/src/test/java/org/knowm/xchange/gateiov4/service/GateioFuturesMarketDataServiceRawTest.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.ExchangeSpecification;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.gateiov4.GateioFuturesExchange;
 import org.knowm.xchange.gateiov4.dto.FuturesSettleType;
 import org.knowm.xchange.gateiov4.dto.marketdata.GateioContractInfo;
 import org.knowm.xchange.gateiov4.dto.marketdata.GateioFundingRate;
 import org.knowm.xchange.utils.Assert;
+import org.knowm.xchange.utils.AuthUtils;
 
 /** Created by lin on 2020-10-16. */
 public class GateioFuturesMarketDataServiceRawTest {
@@ -20,7 +22,21 @@ public class GateioFuturesMarketDataServiceRawTest {
 
   @Before
   public void setUp() throws Exception {
-    exchange = ExchangeFactory.INSTANCE.createExchange(GateioFuturesExchange.class);
+    String HOST = "api.gateio.ws";
+    String URL_API = "https://" + HOST + "/api/v4";
+
+    ExchangeSpecification spec = new ExchangeSpecification(GateioFuturesExchange.class);
+    spec.setSslUri(URL_API);
+    spec.setHost(HOST);
+    spec.setPort(80);
+    spec.setExchangeName("GateioV4");
+    spec.setExchangeDescription("GateioV4 Exchange.");
+    AuthUtils.setApiAndSecretKey(spec, "gateiov4");
+
+    spec.setProxyHost("192.168.1.100");
+    spec.setProxyPort(1083);
+
+    exchange = (GateioFuturesExchange) ExchangeFactory.INSTANCE.createExchange(spec);
     service = (GateioFuturesMarketDataService) exchange.getMarketDataService();
   }
 
